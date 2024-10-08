24aaae53e5f1020d5c99cb66264120113d8496a3
RiseUpGroup23 <154343004+RiseUpGroup23@users.noreply.github.com>
2024-10-08 15:54:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -12,13 +12,6 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json())
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-    next();
-  });
 
 app.get('/', async (req, res ) => {
     res.send("Back andando bien");
